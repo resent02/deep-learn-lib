dac94400e94140a8832f73fadca6f8c259c7ac4f
resent02 <remal11151@gmail.com>
2025-04-02 18:30:32 +0300

    loss and abstract layer class

---
+++ src/loss.py
@@ -0,0 +1,27 @@
+"""
+Loss function measures how good predictions
+"""
+
+import numpy as np
+
+from src.tensor import Tensor
+
+
+class Loss:
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        raise NotImplementedError
+
+    def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+        raise NotImplementedError
+
+
+class MSE(Loss):
+    """
+    Mean Squared error
+    """
+
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        return np.sum((predicted - actual) ** 2)
+
+    def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+        return 2 * (predicted - actual)
---
+++ src/tensor.py
@@ -0,0 +1,5 @@
+"""
+Tensor is n-dimensional array
+"""
+
+from numpy import ndarray as Tensor  # noqa
